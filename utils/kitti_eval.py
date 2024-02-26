d644e62f5de4c41d4b48c5f234c101336623cbdf
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-02-25 18:50:22 -0800

    Just one test

--- utils/kitti_eval.py
+++ utils/kitti_eval.py
@@ -93,8 +93,6 @@ class KITTI_tester():
         self.est = []
         for i, seq in enumerate(self.args.val_seq):
             print(f'testing sequence {seq}')
-            print(seq, type(seq))
-            print(i)
             if seq == "05" and i == 0:
                 print("First")
                 # Added profiler
@@ -103,7 +101,8 @@ class KITTI_tester():
                         pose_est, dec_est, prob_est = self.test_one_path(net, self.dataloader[i], selection, num_gpu=num_gpu, p=p)
                 print(prof.key_averages().table(sort_by="gpu_time_total", row_limit=10))
             else:
-                pose_est, dec_est, prob_est = self.test_one_path(net, self.dataloader[i], selection, num_gpu=num_gpu, p=p)            
+                pass
+                #pose_est, dec_est, prob_est = self.test_one_path(net, self.dataloader[i], selection, num_gpu=num_gpu, p=p)            
             pose_est_global, pose_gt_global, t_rel, r_rel, t_rmse, r_rmse, usage, speed = kitti_eval(pose_est, dec_est, self.dataloader[i].poses_rel)
             
             self.est.append({'pose_est_global':pose_est_global, 'pose_gt_global':pose_gt_global, 'decs':dec_est, 'probs':prob_est, 'speed':speed})
